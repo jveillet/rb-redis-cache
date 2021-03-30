c76132f2f520ddc821aa45e6abfd89121bc3eb73
Jérémie Veillet <jeremie.veillet@gmail.com>
2021-03-30 16:24:21 +0200

    [feature/initial_release] Release first gem version

---
+++ rb-redis-cache.gemspec
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative 'lib/cache/version'
+
+Gem::Specification.new do |spec|
+  spec.name = 'rb-redis-cache'
+  spec.version = Cache::VERSION
+  spec.authors = ['Jérémie Veillet']
+  spec.email = ['jveillet@hey.com']
+  spec.summary = 'A simple framework-agnostic cache with Redis.'
+  spec.description = ''
+  spec.homepage = 'https://github.com/jveillet/rb-redis-cache'
+  spec.license = 'MIT'
+  spec.required_ruby_version = Gem::Requirement.new('>= 2.3.0')
+  spec.platform = Gem::Platform::RUBY
+  spec.metadata['homepage_uri'] = spec.homepage
+  spec.metadata['source_code_uri'] = 'https://github.com/jveillet/rb-redis-cache'
+
+  # Specify which files should be added to the gem when it is released.
+  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
+  spec.files = Dir.chdir(File.expand_path(__dir__)) do
+    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  end
+  spec.bindir = 'exe'
+  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ['lib']
+
+  spec.add_development_dependency 'rake', '~> 12.0'
+  spec.add_development_dependency 'minitest', '~> 5.0'
+  spec.add_development_dependency 'rubocop', '~> 0.91.0'
+  spec.add_development_dependency 'rubocop-performance', '~> 1.6', '>= 1.6.1'
+  spec.add_development_dependency 'yard', '~> 0.9.26'
+
+  spec.add_dependency 'connection_pool', '~> 2.2', '>= 2.2.3'
+  spec.add_dependency 'redis', '~> 4.2', '>= 4.2.5'
+end
---
+++ test/cache_test.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'json'
+
+class CacheTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Cache::VERSION
+  end
+
+  def test_it_writes_into_the_cache
+    result = Cache.write('test/string', 'test')
+    assert result == 'OK'
+  end
+
+  def test_it_reads_into_the_cache
+    Cache.write('test/string', 'test')
+    result = Cache.read('test/string')
+    assert result == 'test'
+  end
+
+  def test_it_doesnt_reads_into_the_cache
+    result = Cache.read('test/string/1234')
+    assert result == nil
+  end
+
+  def test_it_fetch_and_read_into_the_cache
+    Cache.write('test/string', 'test')
+    result = Cache.fetch('test/string')
+    assert result == 'test'
+  end
+
+  def test_it_fetch_and_force_writes_into_the_cache
+    Cache.write('test/string', 'test')
+    result = Cache.fetch('test/string', expires_in: 60, force: true) do
+      'test2'
+    end
+    assert result == 'test2'
+  end
+
+  def test_it_fetch_and_writes_into_the_cache
+    result = Cache.fetch('test/string/2', expires_in: 60) do
+      'test2'
+    end
+    assert result == 'test2'
+  end
+
+  def test_it_checks_key_for_existence
+    Cache.write('test/string', 'test')
+    result = Cache.exists?('test/string')
+    assert result == true
+  end
+
+  def test_it_checks_key_for_non_existence
+    result = Cache.exists?('test/string/1234')
+    assert result == false
+  end
+
+  def test_it_deletes_into_the_cache
+    Cache.write('test/string', 'test')
+    result = Cache.delete('test_key')
+    refute_nil result
+  end
+
+  def test_it_writes_json_into_the_cache
+    message = {a: 'test', b: 'azerty'}
+    result = Cache.write('test/json', message)
+    assert result == 'OK'
+  end
+
+  def test_it_reads_json_into_the_cache
+    test_it_writes_json_into_the_cache
+    result = Cache.read('test/json')
+    assert result[:a] == 'test'
+    assert result[:b] == 'azerty'
+  end
+end
